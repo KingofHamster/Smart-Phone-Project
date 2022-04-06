629d03f432dd9b6f3834166f58a5705caf6446e1
henrytywang <henrytywang@tencent.com>
2022-04-06 17:23:24 +0800

    Refactor, add router

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const db = require('./redis');
-const server = require('./http');
+// const server = require('./http/http');
+const server = require('./http/server');
 
 async function main() {
     await db.init();
-    server.init();
+    server.start();
 
     // todo node_modules local
-    // todo express, Api design, ...
 }
 
 main();
